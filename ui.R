ad396489b46084dd8be288ec6b14ccc2ab68bcf3
gherardovarando <gherardo.varando@gmail.com>
2015-10-29 09:28:25 +0100

    more info + link

--- ui.R
+++ ui.R
@@ -11,14 +11,19 @@ library(leaflet)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Example map old boston"),
+  titlePanel("Example local-tiles"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-     p("Image from:"),
-       p("https://en.wikipedia.org/wiki/History_of_Boston#/media/
-         File:Boston_area_colonial_map.jpg")
+      p("Example by Gherardo Varando"),
+      tags$a("GitHub",href="https://github.com/gherardovarando/example-local-tiles"),
+      p(),
+       tags$a("Original Image",href="https://en.wikipedia.org/wiki/History_of_Boston#/media/
+         File:Boston_area_colonial_map.jpg"),
+      p(),
+      p("The map folder structure is created with the following FIJI/ImageJ plugin:"),
+      tags$a("Map_tools",href="https://github.com/gherardovarando/Map_tools.git")
     ),
 
     # Show a plot of the generated distribution
